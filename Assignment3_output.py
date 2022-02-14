0929594404e0682023ac8bf103b899c719aa86bf
Vaibhav sheokand <92393311+BlipkingBlop@users.noreply.github.com>
2022-02-14 23:10:32 +0530

    Create Assignment3_output.py

---
+++ Assignment3_output.py
@@ -0,0 +1,80 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: /Users/mishankkumar/Desktop/p.py ==================
+QUESTION 1
+ENTER ANY STRING: MISHANK
+{'M': 1, 'I': 1, 'S': 1, 'H': 1, 'A': 1, 'N': 1, 'K': 1}
+
+
+QUESTION 2
+ENTER THE DAY: 14
+ENTER THE MONTH OF THE YEAR: 01
+ENTER THE YEAR: 2002
+15 / 1 / 2002
+
+
+QUESTION 3
+Enter elements of a list separated by space 3 9 10
+list:  3 9 10
+[(3, 9), (9, 81), (10, 100)]
+
+
+QUESTION 4
+Enter Grade Point: 5
+Your Grade is 'C' and Below Average performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 21103107
+Enter Name: MISHANK
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103108
+Enter Name: PARTH
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103109
+Enter Name: SHREY
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103110
+Enter Name: DHRUV
+Do you want to enter another student entry(Y or N):N
+<a> {21103107: 'MISHANK', 21103108: 'PARTH', 21103109: 'SHREY', 21103110: 'DHRUV'}
+<b> {21103110: 'DHRUV', 21103107: 'MISHANK', 21103108: 'PARTH', 21103109: 'SHREY'}
+<c> {21103107: 'MISHANK', 21103108: 'PARTH', 21103109: 'SHREY', 21103110: 'DHRUV'}
+Search Name with SID: 21103108
+<d> PARTH
+
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 10
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 8.8
+
+
+
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
